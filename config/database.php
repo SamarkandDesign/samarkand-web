c26c6c97a2ebe86005883a32a771fd560e38facb
Harry <Harry@Harrys-MacBook-Pro.local>
2016-06-20 23:05:46 +0100

    redis password

--- config/database.php
+++ config/database.php
@@ -141,6 +141,7 @@ return [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'port'     => env('REDIS_PORT', 6379),
             'database' => 0,
+            'password' => env('REDIS_PASSWORD'),
         ],
 
     ],
