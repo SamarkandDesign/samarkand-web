68a9e4195fca327c1d89623d0891e6de5fbce30b
Harry <harry@harryg.me>
2016-07-04 18:58:13 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,38 +10,38 @@ use Illuminate\Support\HtmlString;
 
 class CartController extends Controller
 {
-  /**
-  * Show a list of the cart contents.
-  *
-  * @return \Illuminate\Http\Response
-  */
+    /**
+   * Show a list of the cart contents.
+   *
+   * @return \Illuminate\Http\Response
+   */
   public function index()
   {
-    return view('shop.cart');
+      return view('shop.cart');
   }
 
   /**
-  * Put an item in the cart.
-  *
-  * @param Request $request
-  *
-  * @return \Illuminate\Http\Response
-  */
+   * Put an item in the cart.
+   *
+   * @param Request $request
+   *
+   * @return \Illuminate\Http\Response
+   */
   public function store(AddToCartRequest $request)
   {
-    $product = Product::findOrFail($request->product_id);
-    $qty = (int) $request->quantity;
+      $product = Product::findOrFail($request->product_id);
+      $qty = (int) $request->quantity;
 
-    Cart::associate('Product', 'App')->add([
+      Cart::associate('Product', 'App')->add([
       'id'    => $product->id,
       'qty'   => $qty,
       'name'  => $product->name,
       'price' => $product->getPrice()->asDecimal(),
     ]);
 
-    $request->session()->forget('order');
+      $request->session()->forget('order');
 
-    return redirect()->back()->with([
+      return redirect()->back()->with([
       'alert'       => new HtmlString(sprintf('%d %s added to cart. %s or %s.',
       $qty,
       str_plural($product->name, $qty),
@@ -50,27 +50,27 @@ class CartController extends Controller
     )),
     'alert-class' => 'success',
   ]);
-}
+  }
 
 /**
-* Remove an item from the cart.
-*
-* @param Request $request
-* @param string  $rowid   The row id to remove
-*
-* @return \Illuminate\Http\Response
-*/
+ * Remove an item from the cart.
+ *
+ * @param Request $request
+ * @param string  $rowid   The row id to remove
+ *
+ * @return \Illuminate\Http\Response
+ */
 public function remove(Request $request, $rowid)
 {
-  $product = Cart::get($rowid)->product;
+    $product = Cart::get($rowid)->product;
 
-  Cart::remove($rowid);
+    Cart::remove($rowid);
 
-  $route = Cart::count() > 0 ? 'cart' : 'products.index';
+    $route = Cart::count() > 0 ? 'cart' : 'products.index';
 
-  $request->session()->forget('order');
+    $request->session()->forget('order');
 
-  return redirect()->route($route)->with([
+    return redirect()->route($route)->with([
     'alert'       => "{$product->name} removed from cart",
     'alert-class' => 'success',
   ]);
