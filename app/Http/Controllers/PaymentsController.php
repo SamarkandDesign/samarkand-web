7c6b6888760a03190fbea7e3877b75b3aa119f3c
Harry <harry@harryg.me>
2016-07-13 22:43:23 +0100

    Applied fixes from StyleCI (#33)
    [ci skip] [skip ci]

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -8,64 +8,64 @@ use Illuminate\Http\Request;
 
 class PaymentsController extends Controller
 {
-  /**
-  * Create a new payment for an order.
-  *
-  * @param Request $request
-  *
-  * @return Illuminate\Http\Response
-  */
+    /**
+   * Create a new payment for an order.
+   *
+   * @param Request $request
+   *
+   * @return Illuminate\Http\Response
+   */
   public function store(Request $request, GatewayInterface $gateway)
   {
-    $this->validate($request, ['order_id' => 'required|numeric', 'stripe_token' => 'required']);
-    $order = Order::find($request->order_id);
+      $this->validate($request, ['order_id' => 'required|numeric', 'stripe_token' => 'required']);
+      $order = Order::find($request->order_id);
 
     // Ensure the order being paid for is pending
     if ($order->status !== Order::PENDING) {
-      return redirect()->route('products.index')->with([
-        'alert' => 'Oops, This order has either already been paid for or has been cancelled. Please create a new order to proceed.',
+        return redirect()->route('products.index')->with([
+        'alert'       => 'Oops, This order has either already been paid for or has been cancelled. Please create a new order to proceed.',
         'alert-class' => 'danger',
       ]);
     }
-    
-    try {
-      $charge = $gateway->charge([
+
+      try {
+          $charge = $gateway->charge([
         'amount'      => $order->amount->value(),
         'card'        => $request->stripe_token,
         'description' => sprintf('Order #%s', $order->id),
       ], [
         'email' => $order->email,
       ]);
-    } catch (\App\Billing\CardException $e) {
-      return redirect()->back()->with([
+      } catch (\App\Billing\CardException $e) {
+          return redirect()->back()->with([
         'alert'       => $this->paymentErrorMessage($e->getMessage()),
         'alert-class' => 'danger',
       ]);
-    }
+      }
 
-    $request->session()->forget('order');
-    event(new \App\Events\OrderWasPaid($order, $charge->id));
+      $request->session()->forget('order');
+      event(new \App\Events\OrderWasPaid($order, $charge->id));
 
-    \Cart::destroy();
+      \Cart::destroy();
 
-    $request->session()->flash('order_id', $order->id);
+      $request->session()->flash('order_id', $order->id);
 
-    return redirect()->route('orders.completed');
+      return redirect()->route('orders.completed');
   }
 
   /**
-  * Derive the payment error message.
-  *
-  * @param string $message
-  *
-  * @return string|HtmlString
-  */
+   * Derive the payment error message.
+   *
+   * @param string $message
+   *
+   * @return string|HtmlString
+   */
   private function paymentErrorMessage($message)
   {
-    if (strpos($message, 'zip code')) {
-      return new \Illuminate\Support\HtmlString('The postcode you supplied failed validation, please check your billing address on the <a href="/checkout" class="alert-link" title="return to the checkout page">checkout page</a>.');
-    }
+      if (strpos($message, 'zip code')) {
+          return new \Illuminate\Support\HtmlString('The postcode you supplied failed validation, please check your billing address on the <a href="/checkout" class="alert-link" title="return to the checkout page">checkout page</a>.');
+      }
 
-    return $message;
+      return $message;
   }
 }
