cdc301e8f0811bf6754afa0e2918b49c50d1727d
Harry <harry@harryg.me>
2016-07-23 07:48:54 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,17 +5,17 @@ namespace App\Providers;
 use App\Billing\GatewayInterface;
 use App\Billing\StripeGateway;
 use App\Page;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Logging\Log;
+use Illuminate\Support\ServiceProvider;
 use Psr\Log\LoggerInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-    * Bootstrap any application services.
-    *
-    * @return void
-    */
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
         $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local' and $this->app->environment() != 'testing');
@@ -40,18 +40,18 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-    * Register any application services.
-    *
-    * This service provider is a great spot to register your various container
-    * bindings with the application. As you can see, we are registering our
-    * "Registrar" implementation here. You can add your own bindings too!
-    *
-    * @return void
-    */
+     * Register any application services.
+     *
+     * This service provider is a great spot to register your various container
+     * bindings with the application. As you can see, we are registering our
+     * "Registrar" implementation here. You can add your own bindings too!
+     *
+     * @return void
+     */
     public function register()
     {
         // Bugsnag reporting
-        if($this->app->environment('production')) {
+        if ($this->app->environment('production')) {
             $this->app->alias('bugsnag.multi', Log::class);
             $this->app->alias('bugsnag.multi', LoggerInterface::class);
         }
