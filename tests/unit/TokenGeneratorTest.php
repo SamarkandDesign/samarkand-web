0a164ce581184fda32b965c89b563e119841dca6
Harry <harry@harryg.me>
2016-08-17 00:13:01 +0100

    Applied fixes from StyleCI (#60)
    [ci skip] [skip ci]

--- tests/unit/TokenGeneratorTest.php
+++ tests/unit/TokenGeneratorTest.php
@@ -2,18 +2,19 @@
 
 use App\Search\TokenGenerator;
 
-class TokenGeneratorTest extends TestCase {
-  
-  /** @test */
-  public function it_generates_a_token() {
-    SearchIndex::shouldReceive('getClient')->once()->andReturn(
+class TokenGeneratorTest extends TestCase
+{
+    /** @test */
+  public function it_generates_a_token()
+  {
+      SearchIndex::shouldReceive('getClient')->once()->andReturn(
       Mockery::mock([
-        'generateSecuredApiKey' => 'abc123'
+        'generateSecuredApiKey' => 'abc123',
         ])
       );
 
-    $generator = App::make(TokenGenerator::class);
+      $generator = App::make(TokenGenerator::class);
 
-    $this->assertNotEmpty($generator->getToken());
+      $this->assertNotEmpty($generator->getToken());
   }
 }
