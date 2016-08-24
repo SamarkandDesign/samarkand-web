ec12e39d4c2ed7a03d7f3f5097995e68417cfdbd
Harry <harry@harryg.me>
2016-08-24 22:50:04 +0100

    Applied fixes from StyleCI (#63)
    [ci skip] [skip ci]

--- tests/unit/TokenGeneratorTest.php
+++ tests/unit/TokenGeneratorTest.php
@@ -8,15 +8,15 @@ class TokenGeneratorTest extends TestCase
     /** @test */
   public function it_generates_a_token()
   {
-    // override the real algolia client
+      // override the real algolia client
     App::singleton(Client::class, function () {
         return Mockery::mock(Client::class, [
       'generateSecuredApiKey' => 'abc123',
       ]);
     });
 
-    $generator = App::make(TokenGenerator::class);
+      $generator = App::make(TokenGenerator::class);
 
-    $this->assertEquals('abc123', $generator->getProductSearchToken());
+      $this->assertEquals('abc123', $generator->getProductSearchToken());
   }
 }
