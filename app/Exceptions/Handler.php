29450caf9e36da8375490237dea18d69a985333c
Harry <harry@harryg.me>
2016-08-25 14:59:30 +0100

    Don't report Authentication exception
    Fixes #66

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,10 +18,12 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        AuthorizationException::class,
-        HttpException::class,
-        ModelNotFoundException::class,
-        ValidationException::class,
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
     ];
 
     /**
