c66de9b218c0242f853505580cf607efb20fb5d3
Harry <harry@harryg.me>
2016-11-14 21:49:43 +0000

    Applied fixes from StyleCI (#114)
    [ci skip] [skip ci]

--- app/Services/Invoicing/XeroInvoiceCreator.php
+++ app/Services/Invoicing/XeroInvoiceCreator.php
@@ -10,41 +10,43 @@ use XeroPHP\Models\Accounting\Invoice\LineItem;
 
 class XeroInvoiceCreator implements InvoiceCreator
 {
-  const ACCOUNT_CODE = '200';
-  const INVOICE_STATUS = 'AUTHORISED';
-  const INVOICE_TYPE = 'ACCREC';
+    const ACCOUNT_CODE = '200';
+    const INVOICE_STATUS = 'AUTHORISED';
+    const INVOICE_TYPE = 'ACCREC';
 
-  protected $xero;
-  
-  public function __construct(PrivateApplication $xero)
-  {
-    $this->xero = $xero;
-  }
+    protected $xero;
+
+    public function __construct(PrivateApplication $xero)
+    {
+        $this->xero = $xero;
+    }
 
   /**
-   * Create an invoice for an order
-   * @param  Order  $order 
-   * @return String        The ID of the saved invoice
+   * Create an invoice for an order.
+   *
+   * @param  Order  $order
+   *
+   * @return string        The ID of the saved invoice
    */
   public function createInvoice(Order $order)
   {
-    if (!is_null($order->invoice_id)) {
-      throw new \Exception(sprintf('The order has already an invoice generated for it ("%s")', $order->invoice_id));
-    }
+      if (!is_null($order->invoice_id)) {
+          throw new \Exception(sprintf('The order has already an invoice generated for it ("%s")', $order->invoice_id));
+      }
 
-    $invoice = $this->buildInvoice($order);
-    $invoice->save();
+      $invoice = $this->buildInvoice($order);
+      $invoice->save();
 
-    return $invoice->getInvoiceId();
+      return $invoice->getInvoiceId();
   }
 
-  protected function buildInvoice(Order $order)
-  {
-    $contact = (new Contact())->setName($order->user->name)
+    protected function buildInvoice(Order $order)
+    {
+        $contact = (new Contact())->setName($order->user->name)
     ->setEmailAddress($order->user->email);
 
-    $invoice = new Invoice($this->xero);
-    $invoice = $invoice->setType(self::INVOICE_TYPE)
+        $invoice = new Invoice($this->xero);
+        $invoice = $invoice->setType(self::INVOICE_TYPE)
     ->setStatus(self::INVOICE_STATUS)
     ->setDate($order->updated_at)
     ->setDueDate($order->updated_at)
@@ -53,20 +55,20 @@ class XeroInvoiceCreator implements InvoiceCreator
     ->setLineAmountType('Inclusive');
 
 
-    foreach ($this->getLineItems($order) as $item) {
-      $invoice->addLineItem($item);
+        foreach ($this->getLineItems($order) as $item) {
+            $invoice->addLineItem($item);
+        }
+
+        return $invoice;
     }
 
-    return $invoice;
-  }
-
-  protected function getLineItems($order)
-  {
-    return $order->items->map(function ($item) {
-      return (new LineItem())->setDescription($item->description)
+    protected function getLineItems($order)
+    {
+        return $order->items->map(function ($item) {
+            return (new LineItem())->setDescription($item->description)
       ->setQuantity($item->quantity)
       ->setUnitAmount($item->price_paid->asDecimal())
       ->setAccountCode(self::ACCOUNT_CODE);
-    });
-  }
+        });
+    }
 }
--- tests/unit/InvoiceCreatorTest.php
+++ tests/unit/InvoiceCreatorTest.php
@@ -17,34 +17,32 @@ class InvoiceCreatorTest extends TestCase
   /** @test **/
   public function it_creates_an_invoice_from_an_order_and_returns_the_id()
   {
+      $invoiceCreator = new XeroInvoiceCreator(new FakePrivateXeroApplication());
 
-    $invoiceCreator = new XeroInvoiceCreator(new FakePrivateXeroApplication);
+      $order = $this->makeOrder();
 
-    $order = $this->makeOrder();
-
-    $invoiceId = $invoiceCreator->createInvoice($order);
-    $this->assertEquals(FakePrivateXeroApplication::INVOICE_ID, $invoiceId);
-
+      $invoiceId = $invoiceCreator->createInvoice($order);
+      $this->assertEquals(FakePrivateXeroApplication::INVOICE_ID, $invoiceId);
   }
 
   /** @test **/
   public function it_takes_exception_to_an_order_that_already_has_an_invoice()
   {
-    $invoiceCreator = new XeroInvoiceCreator(new FakePrivateXeroApplication);
-    $order = $this->makeOrder();
-    $order->update(['invoice_id' => FakePrivateXeroApplication::INVOICE_ID]);
+      $invoiceCreator = new XeroInvoiceCreator(new FakePrivateXeroApplication());
+      $order = $this->makeOrder();
+      $order->update(['invoice_id' => FakePrivateXeroApplication::INVOICE_ID]);
 
-    $this->setExpectedException(Exception::class);
+      $this->setExpectedException(Exception::class);
 
-    $invoiceId = $invoiceCreator->createInvoice($order);
+      $invoiceId = $invoiceCreator->createInvoice($order);
   }
 
-  protected function makeOrder()
-  {
-    $order = factory(Order::class)->create();
-    $items = factory(OrderItem::class, 3)->create();
-    $order->order_items()->saveMany($items);
-    return $order;
-  }
+    protected function makeOrder()
+    {
+        $order = factory(Order::class)->create();
+        $items = factory(OrderItem::class, 3)->create();
+        $order->order_items()->saveMany($items);
 
+        return $order;
+    }
 }
