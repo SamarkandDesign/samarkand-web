558d08d920ffe75160142ed964695bd030182150
Harry <harry@harryg.me>
2018-02-24 17:48:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,7 +21,7 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
         parent::tearDown();
         $refl = new ReflectionObject($this);
         foreach ($refl->getProperties() as $prop) {
-            if (!$prop->isStatic() && 0 !== strpos($prop->getDeclaringClass()->getName(), 'PHPUnit_')) {
+            if (! $prop->isStatic() && 0 !== strpos($prop->getDeclaringClass()->getName(), 'PHPUnit_')) {
                 $prop->setAccessible(true);
                 $prop->setValue($this, null);
             }
