e522f2ad1793a9919174463cb29f151838f39197
harrygr <harry@grumbar.me>
2018-12-02 16:12:41 +0000

    Add honeypot field to contact form

--- resources/views/partials/alerts/_errors_block.blade.php
+++ resources/views/partials/alerts/_errors_block.blade.php
@@ -1,5 +1,5 @@
 @if (isset($errors) and count($errors) > 0)
-<alert type="danger" block="true">
+<alert type="danger" :block="true">
 		<strong>Whoops!</strong> There were some problems with your input.<br><br>
 		<ul>
 			@foreach ($errors->all() as $error)
