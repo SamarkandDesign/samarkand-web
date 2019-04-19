a0b06e4ddf57b9d69f3e019b4aa95bfab1c3d09a
harrygr <harry@bulb.co.uk>
2019-04-19 12:29:38 +0100

    Create ajax-powered feedback form

---
+++ app/Http/Controllers/Api/FeedbacksController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Feedback;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Mail\ContactSubmitted;
+
+class FeedbacksController extends Controller
+{
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'message' => 'required|max:255',
+      'order_id' => 'required',
+    ]);
+
+    $user = $request->user();
+
+    $feedback = Feedback::create([
+      'message' => $request->get('message'),
+      'user_id' => $user ? $user()->id : 1,
+    ]);
+
+    return response()->json($feedback, Response::HTTP_CREATED);
+  }
+}
---
+++ resources/assets/js/components/skd-feedback.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-sm-8 top-buffer">
+        <div role="alert" class="alert alert-danger" v-if="this.errorMessage">
+          <p>{{ this.errorMessage }}</p>
+        </div>
+
+        <div
+          role="alert"
+          class="top-buffer alert alert-success alert-block clearfix"
+          v-if="submitted"
+        >
+          Thanks for your feedback.
+          <a href="/" class="btn btn-primary pull-right">Back to home page</a>
+        </div>
+
+        <form
+          v-bind:action="route"
+          method="POST"
+          class="top-buffer"
+          @submit.prevent="submitForm"
+          v-if="!submitted"
+        >
+          <div class="form-group" v-bind:class="{'has-error': errors.message}">
+            <label class="control-label" for="message">Where did you hear about us?</label>
+            <textarea
+              id="message"
+              class="form-control"
+              rows="5"
+              name="message"
+              v-model="form.message"
+            ></textarea>
+            <span
+              v-if="errors.message"
+              class="text-danger"
+            >{{ errors.message ? errors.message[0] : '' }}</span>
+          </div>
+
+          <button type="submit" class="btn btn-primary" id="send">
+            <span v-show="loading">
+              <i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Loading...
+            </span>
+            <span v-show="!loading">Submit</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { client } from '../client';
+
+export default {
+  props: {
+    route: { type: String, required: true },
+    orderId: { required: true },
+  },
+  data() {
+    return {
+      form: {
+        message: '',
+        order_id: this.orderId,
+      },
+      errors: {},
+      errorMessage: '',
+      loading: false,
+      submitted: false,
+    };
+  },
+
+  computed: {
+    hasErrors() {
+      return Object.keys(this.errors).length > 0 || this.errorMessage.length;
+    },
+  },
+  methods: {
+    submitForm() {
+      this.errorMessage = '';
+      this.loading = true;
+      this.errors = {};
+      this.errorMessage = '';
+      client
+        .post(this.route, this.form)
+        .then(response => {
+          this.loading = false;
+          this.submitted = true;
+        })
+        .catch(err => {
+          this.loading = false;
+
+          if (err.response && typeof err.response.data === 'object') {
+            this.errors = err.response.data;
+          } else {
+            this.errorMessage =
+              'Oops, it looks like something went wrong saving your submission. Please try again.';
+          }
+
+          console.log(this.errors);
+        });
+    },
+  },
+};
+</script>
