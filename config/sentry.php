016f9a00e84f91f41ad764970cc1ebd4d79699f1
harrygr <harry@bulb.co.uk>
2019-05-15 17:44:20 +0100

    Add release to sentry config

--- config/sentry.php
+++ config/sentry.php
@@ -6,6 +6,8 @@ return [
   // capture release as git sha
   // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
+  'release' => sprintf("samarkand@%s", env('HEROKU_RELEASE_VERSION', 'unknown')),
+
   'breadcrumbs' => [
     // Capture bindings on SQL queries logged in breadcrumbs
     'sql_bindings' => true,
