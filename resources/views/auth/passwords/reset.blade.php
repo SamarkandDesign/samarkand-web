a986e15d3965fa8dbe9f2de4d72fe7ff551da73b
harrygr <harry@bulb.co.uk>
2019-07-07 10:06:57 +0100

    Fix broken password reset

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -8,38 +8,39 @@ Reset Password
 <h1 class="text-center">Reset Password</h1>
 
 
-	<form  role="form" method="POST" action="/password/reset"  class="vspace-5">
-		<input type="hidden" name="_token" value="{{ csrf_token() }}">
+<form role="form" method="POST" action="/password/reset" class="vspace-5">
+	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	<input type="hidden" name="token" value="{{ $token }}">
 
-		<div class="form-group">
-			<label class="col-md-4 control-label">Email address</label>
-			<div class="col-md-6">
-				<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-			</div>
+	<div class="form-group">
+		<label class="col-md-4 control-label">Email address</label>
+		<div class="col-md-6">
+			<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 		</div>
+	</div>
 
-		<div class="form-group">
-			<label class="col-md-4 control-label">Password</label>
-			<div class="col-md-6">
-				<input type="password" class="form-control" name="password">
-			</div>
+	<div class="form-group">
+		<label class="col-md-4 control-label">Password</label>
+		<div class="col-md-6">
+			<input type="password" class="form-control" name="password">
 		</div>
+	</div>
 
-		<div class="form-group">
-			<label class="col-md-4 control-label">Confirm password</label>
-			<div class="col-md-6">
-				<input type="password" class="form-control" name="password_confirmation">
-			</div>
+	<div class="form-group">
+		<label class="col-md-4 control-label">Confirm password</label>
+		<div class="col-md-6">
+			<input type="password" class="form-control" name="password_confirmation">
 		</div>
+	</div>
 
-		<div class="form-group">
-			<div class="col-md-6 col-md-offset-4">
-				<button type="submit" class="btn btn-primary">
-					Reset password
-				</button>
-			</div>
+	<div class="form-group">
+		<div class="col-md-6 col-md-offset-4">
+			<button type="submit" class="btn btn-primary">
+				Reset password
+			</button>
 		</div>
-	</form>
+	</div>
+</form>
 </div>
 
 @endsection
